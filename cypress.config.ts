5a3c1d47a4df103017a0559dd44c11c93193c9b3
Ferin.Chow <nrrri.cho@gmail.com>
2026-03-22 16:53:21 -0400

    fix: update url

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,15 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:8080",
+    baseUrl:
+      process.env.CYPRESS_BASE_URL ||
+      "https://takehome-desktop.d.tekvisionflow.com",
     specPattern: "cypress/e2e/**/*.cy.ts",
     supportFile: "cypress/support/e2e.ts",
+  },
+  env: {
+    BASE_URL:
+      process.env.CYPRESS_BASE_URL ||
+      "https://takehome-desktop.d.tekvisionflow.com",
   },
 });
